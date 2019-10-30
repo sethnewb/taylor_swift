2eef2eb67848ac19d2b5b5bb7ccb9884228470ae
Seth <seth.newbill@temperpack.com>
2019-10-30 12:19:04 -0400

    small changes

--- t_swift.py
+++ t_swift.py
@@ -1,13 +1,16 @@
 import markovify as mrk
 
 chorus = []
+bridge = []
 
 with open("all_tswift_lyrics.txt") as f:
     text = f.read()
 
 text_model = mrk.Text(text)
 
-
+print("")
+print("")
+print("[VERSE 1]")
 for i in range(5):
     print(text_model.make_sentence())
 
@@ -19,9 +22,24 @@ for i in range(3):
     print(line)
 
 print("")
-print("[VERSE]")
+print("[VERSE 2]")
 for i in range(5):
     print(text_model.make_sentence())
+
+print("")
+print("[CHORUS]")
+for i in chorus:
+    print(i)
+
+print("")
+print("[BRIDGE]")
+for i in range(2):
+    line = text_model.make_short_sentence(120)
+    bridge.append(line)
+    print(line)
+
+for i in bridge:
+    print(i)
 
 print("")
 print("[CHORUS]")
