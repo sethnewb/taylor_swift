6d5c058938a1c6702f6760f7db8af4188b4c874e
Seth <seth.newbill@temperpack.com>
2020-04-23 13:50:54 -0400

    tfb

--- song_writer.py
+++ song_writer.py
@@ -1,30 +1,37 @@
 import markovify as mrk
 
+swift = "all_tswift_lyrics.txt"
+tfb = "tfb.txt"
+
+
 chorus = []
 bridge = []
 
-with open("all_tswift_lyrics.txt") as f:
+with open(tfb) as f:
     text = f.read()
 
 text_model = mrk.Text(text)
+
+title = text_model.make_short_sentence(35)
+print('Title:' + title)
 
 print("")
 print("")
 print("[VERSE 1]")
 for i in range(5):
-    print(text_model.make_sentence())
+    print(text_model.make_short_sentence(100))
 
 print("")
 print("[CHORUS]")
 for i in range(3):
-    line = text_model.make_short_sentence(280)
+    line = text_model.make_short_sentence(200)
     chorus.append(line)
     print(line)
 
 print("")
 print("[VERSE 2]")
 for i in range(5):
-    print(text_model.make_sentence())
+    print(text_model.make_short_sentence(150))
 
 print("")
 print("[CHORUS]")
@@ -45,3 +52,10 @@ print("")
 print("[CHORUS]")
 for i in chorus:
     print(i)
+
+print("")
+print("[HOOK]")
+for i in range(1):
+    line = text_model.make_short_sentence(80)
+    bridge.append(line)
+    print(line + '[x8]')
